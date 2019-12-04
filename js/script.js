438b154f91f5dadd5c1db7c0919dce700e6a8973
alysonmiguel <alysonmiguel16@gmail.com>
2019-12-04 19:56:06 -0200

    Telas de home, contato e equipe ok

--- js/script.js
+++ js/script.js
@@ -6,48 +6,52 @@ let textarea = document.getElementById("textarea").value;
 let erro = [];
 
 function verificar() {
-    let ready = true;
-    username = document.getElementById("username").value;
-    email = document.getElementById("email").value;
-    assunto = document.getElementById("assunto").value;
-    textarea = document.getElementById("textarea").value;
+  let ready = true;
+  username = document.getElementById("username").value;
+  email = document.getElementById("email").value;
+  assunto = document.getElementById("assunto").value;
+  textarea = document.getElementById("textarea").value;
 
-    if (!username || !email || !assunto || !textarea){
-        ready = false;
-        erro.push("Os campos são obrigatórios");
-    }
+  if (!username || !email || !assunto || !textarea) {
+    ready = false;
+    erro.push("Os campos são obrigatórios");
+  }
 
-    if (username.length < 4) {
-        ready = false;
-        document.getElementById("erroNome").innerText= "Usuario deve ter pelo menos de 4 letras"
-    }
-    if (!email.includes("@")) {
-        ready = false
-        erro.push("Esse não é um email valido");
-    }
-    if (assunto.length < 5) {
-        ready = false
-        erro.push("O assunto deve possuir mais de 5 caracteres");
-    }
-    if (textarea.length <= 20) {
-        erro.push("A mensagem deve possuir no mínimo 20 caracteres");
-        ready = false
-    };
+  if (username.length < 4) {
+    ready = false;
+    document.getElementById("erroNome").innerText =
+      "Usuario deve ter pelo menos de 4 letras";
+  }
+  if (!email.includes("@")) {
+    ready = false;
+    document.getElementById("erroEmail").innerText =
+      "Esse não é um email valido";
+  }
+  if (assunto.length < 5) {
+    ready = false;
+    document.getElementById("erroAssunto").innerText =
+      "O assunto deve possuir mais de 5 caracteres";
+  }
+  if (textarea.length <= 20) {
+    document.getElementById("erroMsg").innerText =
+      "A mensagem deve possuir no mínimo 20 caracteres";
+    ready = false;
+  }
 
-    sendForm(ready)
+  sendForm(ready);
 }
 
-
 function sendForm(ready) {
-    document.getElementById("formulario").addEventListener("click", function (event) {
-      event.preventDefault()    
+  document
+    .getElementById("formulario")
+    .addEventListener("click", function(event) {
+      event.preventDefault();
     });
-    if (ready) {
-      alert("Envio bem sucedido!")
-      } else {
-            console.log(erro);
-            alert("algo ta errado folks")
-            erro = [];
-      }
-      
-    }
+  if (ready) {
+    alert("Envio bem sucedido!");
+  } else {
+    console.log(erro);
+    alert("algo ta errado folks");
+    erro = [];
+  }
+}
