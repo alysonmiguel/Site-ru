9abd49a642979183e736df571cfec6ef329dbc25
alysonmiguel <alysonmiguel16@gmail.com>
2019-12-04 23:18:03 -0200

    Construção da pagina de satisfação

--- js/script.js
+++ js/script.js
@@ -41,6 +41,30 @@ function verificar() {
   sendForm(ready);
 }
 
+function verificarSatisfacao() {
+  let ready = true;
+  username = document.getElementById("username").value;
+  textarea = document.getElementById("textarea").value;
+
+  if (!username || !textarea) {
+    ready = false;
+    erro.push("Os campos são obrigatórios");
+  }
+
+  if (username.length < 4) {
+    ready = false;
+    document.getElementById("erroNome").innerText =
+      "Usuario deve ter pelo menos de 4 letras";
+  }
+  if (textarea.length <= 20) {
+    document.getElementById("erroMsg").innerText =
+      "A mensagem deve possuir no mínimo 20 caracteres";
+    ready = false;
+  }
+
+  sendForm(ready);
+}
+
 function sendForm(ready) {
   document
     .getElementById("formulario")
